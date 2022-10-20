66f2589201b9a96ba775872569ca31361ceeef58
Victor Olsson <victor.olssn@gmail.com>
2022-10-20 21:09:02 +0200

    add docker

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,10 @@
 FROM python:3.9
 
+# cv2 dependencies
+RUN apt-get update && apt-get install libgl1 -y && apt-get install net-tools -y
+
 WORKDIR /app
-COPY . ./
+#COPY . ./
+COPY requirements.txt .
 
 RUN pip install -r requirements.txt
-EXPOSE 5000 
-EXPOSE 8080
--- read_stream.py
+++ read_stream.py
@@ -1,31 +1,22 @@
-import os
 import argparse
-import time
 import logging
-from typing import Tuple
-import numpy as np
+import os
+import time
+
 import cv2
+import numpy as np
+import paho.mqtt.client as mqtt
 from cv2 import VideoCapture
 from cvzone.HandTrackingModule import HandDetector
-import paho.mqtt.client as mqtt
 
-mqtt_broker_ip = "192.168.0.116"
+print(os.environ["MQTT_BROKER_IP"])
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
 
-""" TODO: 
-        - find faces in img
-        - Do facerecognition on faces
-        - Create webpage where stream can be seen
-        - Save data for x hours. But maybe only data where faces exist? also save faces found and link to time in video
-        - Create python lib
-        - Run on server. Docker?
-""" 
-
 def get_player_from_ip_camera(rtsp_url: str) -> VideoCapture:
-    """ Generate player
+    """Generate player
 
     Args:
         rtsp_url (str): rtsp_url as "rtsp://user:pw@camera_ip"
@@ -35,11 +26,13 @@ def get_player_from_ip_camera(rtsp_url: str) -> VideoCapture:
     """
     return cv2.VideoCapture(rtsp_url)
 
+
 def get_player_from_webcam():
     return cv2.VideoCapture(0)
 
-def generate_rtsp_url(ip: str, hq: bool=True) -> str:
-    """ Generate correct ip format, i.e. "rtsp://user:pw@ip".
+
+def generate_rtsp_url(ip: str, hq: bool = True) -> str:
+    """Generate correct ip format, i.e. "rtsp://user:pw@ip".
         Environment variables CAMERA_USER and CAMERA_PW is needed.
 
     Args:
@@ -60,6 +53,7 @@ def generate_rtsp_url(ip: str, hq: bool=True) -> str:
         stream = "stream2"
     return f"rtsp://{user}:{pw}@{ip}/{stream}"
 
+
 def stream_video(ip: str, frame_rate: int = 2) -> None:
     if not DEBUG:
         rtsp_url = generate_rtsp_url(ip)
@@ -68,61 +62,68 @@ def stream_video(ip: str, frame_rate: int = 2) -> None:
         player = get_player_from_webcam()
 
     logger.info("loading face detector...")
-    #detector = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
-    prev = 0
+    # detector = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+    prev: float = 0
     detector = HandDetector(detectionCon=0.8, maxHands=2)
     if not DEBUG:
-        client = mqtt.Client(client_id=mqtt_broker_ip, clean_session=True, userdata=None, protocol=mqtt.MQTTv311, transport="tcp")
+        client = mqtt.Client(
+            client_id=os.environ["MQTT_BROKER_IP"],
+            clean_session=True,
+            userdata=None,
+            protocol=mqtt.MQTTv311,
+            transport="tcp",
+        )
         client.username_pw_set(username=os.environ["MQTT_USER"], password=os.environ["MQTT_PW"])
-        client.connect(mqtt_broker_ip)
+        client.connect(os.environ["MQTT_BROKER_IP"])
 
     while player.isOpened():
         time_elapsed = time.time() - prev
-        _, img  = player.read()
-        if time_elapsed > 1./frame_rate:
+        _, img = player.read()
+        if time_elapsed > 1.0 / frame_rate:
             img = rescale_frame(img, percent=50)
             prev = time.time()
-            #faces = haar_find_faces(frame, detector)
-            #if len(faces) > 0:
+            # faces = haar_find_faces(frame, detector)
+            # if len(faces) > 0:
             #    print(faces)
             #    frame = plot_haar_faces(frame, faces)
             hands, img = detector.findHands(img)  # with draw
             if hands:
                 # Hand 1
                 hand = hands[0]
-                lmList1 = hand["lmList"]  # List of 21 Landmark points
-                bbox1 = hand["bbox"]  # Bounding box info x,y,w,h
-                centerPoint1 = hand['center']  # center of the hand cx,cy
-                handType1 = hand["type"]  # Handtype Left or Right
+                # lmList1 = hand["lmList"]  # List of 21 Landmark points
+                # bbox1 = hand["bbox"]  # Bounding box info x,y,w,h
+                # centerPoint1 = hand["center"]  # center of the hand cx,cy
+                # handType1 = hand["type"]  # Handtype Left or Right
 
                 fingers = detector.fingersUp(hand)
                 logger.info(f"Number of fingers: {sum(fingers)}")
                 logger.info(fingers)
                 msg = sum(fingers)
-                print(sum(fingers))
             else:
                 msg = "unavailable"
             if not DEBUG:
                 client.publish("home/camera/number_of_fingers", msg)
 
+            if DEBUG:
+                try:
+                    cv2.imshow("Wyze v2 camera", img)
+                except cv2.error as e:
+                    logger.warning(e)
 
-            try:
-                cv2.imshow("Wyze v2 camera", img)
-            except cv2.error as e:
-                logger.warning(e)
-
-            # Quit when 'x' is pressed
-            if cv2.waitKey(1) & 0xFF == ord('x'):
-                break
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                break
+                # Quit when 'x' is pressed
+                if cv2.waitKey(1) & 0xFF == ord("x"):
+                    break
+                if cv2.waitKey(1) & 0xFF == ord("q"):
+                    break
     player.release()
 
+
 def rescale_frame(frame, percent=75):
-    width = int(frame.shape[1] * percent/ 100)
-    height = int(frame.shape[0] * percent/ 100)
+    width = int(frame.shape[1] * percent / 100)
+    height = int(frame.shape[0] * percent / 100)
     dim = (width, height)
-    return cv2.resize(frame, dim, interpolation =cv2.INTER_AREA)
+    return cv2.resize(frame, dim, interpolation=cv2.INTER_AREA)
+
 
 def plot_haar_faces(frame, faces):
     for (x, y, w, h) in faces:
@@ -130,27 +131,19 @@ def plot_haar_faces(frame, faces):
         frame = cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
     return frame
 
+
 def haar_find_faces(frame, detector) -> np.ndarray:
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     # perform face detection
-    return detector.detectMultiScale(gray, scaleFactor=1.25,
-                                      minNeighbors=5, minSize=(64, 64),
-                                      flags=cv2.CASCADE_SCALE_IMAGE)
+    return detector.detectMultiScale(
+        gray, scaleFactor=1.25, minNeighbors=5, minSize=(64, 64), flags=cv2.CASCADE_SCALE_IMAGE
+    )
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument(
-        '--debug',
-        action='store_true', 
-        help='print debug messages to stderr'
-    )
+    parser.add_argument("--debug", action="store_true", help="print debug messages to stderr")
     args = parser.parse_args()
     DEBUG = args.debug
 
-
-    ip = "192.168.0.128"
-    stream_video(ip)
-    
-    
-
+    stream_video(os.environ["CAMERA_IP"])
