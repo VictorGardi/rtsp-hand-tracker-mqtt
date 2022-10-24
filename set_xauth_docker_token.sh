93311835bb8f75ab7da44ed4b5265329b551ba60
Victor Olsson <victor.olssn@gmail.com>
2022-10-24 18:47:28 +0200

    create docker-compose.qa.yml and update bash script

--- set_xauth_docker_token.sh
+++ set_xauth_docker_token.sh
@@ -3,11 +3,11 @@ DISPLAY_NUMBER=$(echo $DISPLAY | cut -d. -f1 | cut -d: -f2)
 
 # Extract auth cookie
 AUTH_COOKIE=$(xauth list | grep "^$(hostname)/unix:${DISPLAY_NUMBER} " | awk '{print $3}')
-if [[ ! $AUTH_COOKIE ]] then
+if [[ ! $AUTH_COOKIE ]]; then
     AUTH_COOKIE=$(xauth list | grep "^$(hostname)/unix: " | awk '{print $3}')
 fi
-if [[ ! $AUTH_COOKIE ]] then
-    printf '%s\n' "It seemsl like the current device don't have a device! Debug mode is not possible." >&2
+if [[ ! $AUTH_COOKIE ]]; then
+    printf '%s\n' "It seems like the current device doesn't have a display! Debug mode is not possible." >&2
     exit 1
 fi
 # Add the xauth cookie to xauth
